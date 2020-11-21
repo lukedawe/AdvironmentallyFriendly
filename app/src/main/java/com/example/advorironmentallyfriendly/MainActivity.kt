7e772a1a3230d590eb0777dab7d4c1cac2a1c19d
Luke <luke.dawe@gmail.com>
2020-11-21 15:35:16 +0000

    Ads might be working?
    Signed-off-by: Luke <luke.dawe@gmail.com>

--- app/src/main/java/com/example/advorironmentallyfriendly/MainActivity.kt
+++ app/src/main/java/com/example/advorironmentallyfriendly/MainActivity.kt
@@ -1,16 +1,16 @@
 package com.example.advorironmentallyfriendly
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.ads.MobileAds;
+
 
 class MainActivity : AppCompatActivity() {
-    //AdView adView1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        // adView1 = findViewById(R.id.ad_view1)
-    }
 
+        MobileAds.initialize(this) {}
+    }
 
 }
