bc3c0825849273cc3e10b5a00fff073e2568e559
Luke <luke.dawe@gmail.com>
2020-11-21 15:07:33 +0000

    Getting the ads working with firebase
    Signed-off-by: Luke <luke.dawe@gmail.com>

--- app/src/main/java/com/example/advorironmentallyfriendly/MainActivity.kt
+++ app/src/main/java/com/example/advorironmentallyfriendly/MainActivity.kt
@@ -2,12 +2,15 @@ package com.example.advorironmentallyfriendly
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
+    //AdView adView1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // adView1 = findViewById(R.id.ad_view1)
     }
+
 
 }
