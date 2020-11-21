290e9535412aa3bfa0da90f4a90d96df1a8566ae
James <jamesshaw112@sky.com>
2020-11-21 17:37:26 +0000

    Working with ads

---
+++ app/src/main/java/com/example/advironmentallyfriendly/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.advironmentallyfriendly;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AdView mAdView;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+    }
+}
