faddf6bfd8f7b354101320870d679cac47b381ce
Luke <luke.dawe@gmail.com>
2020-11-22 00:54:36 +0000

    NOT STABLE

--- app/src/main/java/com/example/advironmentallyfriendly/ScreenSlidePageFragment.java
+++ app/src/main/java/com/example/advironmentallyfriendly/ScreenSlidePageFragment.java
@@ -13,10 +13,9 @@ import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.PagerAdapter;
 
 public class ScreenSlidePageFragment extends Fragment {
-
-
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         ViewGroup rootView = (ViewGroup) inflater.inflate(
                 R.layout.fragment_screen_slide_page, container, false);
 
