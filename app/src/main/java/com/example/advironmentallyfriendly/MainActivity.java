7a6d28e77d7e8c92117f1d7d7a96be19537cf324
James <jamesshaw112@sky.com>
2020-11-23 12:07:04 +0000

    Final fix with real ads

--- app/src/main/java/com/example/advironmentallyfriendly/MainActivity.java
+++ app/src/main/java/com/example/advironmentallyfriendly/MainActivity.java
@@ -1,4 +1,10 @@
 package com.example.myapplication;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -7,16 +13,27 @@ import android.widget.ImageView;
 import android.widget.ViewFlipper;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = "MainActivity";
     ViewFlipper v_flipper;
+    private AdView mAdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+
         // Images in drawable
-        int images[] = {R.drawable.group1,R.drawable.group2,R.drawable.group3};
+        int images[] = {R.drawable.page01,R.drawable.page02,R.drawable.page03,R.drawable.page04,R.drawable.page05,R.drawable.page06,R.drawable.page07,R.drawable.page08,R.drawable.page09,R.drawable.page10,R.drawable.page11,R.drawable.page12,R.drawable.page13,R.drawable.page15,R.drawable.page16,R.drawable.page18,R.drawable.page19};
 
         v_flipper= findViewById(R.id.v_flipper);
 
@@ -31,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
 
         v_flipper.addView(imageView);
         // Changes every X seconds so change this number
-        v_flipper.setFlipInterval(1000);
+        v_flipper.setFlipInterval(10000);
         v_flipper.setAutoStart(true);
         v_flipper.setInAnimation(this,android.R.anim.slide_in_left);
         v_flipper.setOutAnimation(this,android.R.anim.slide_out_right);
